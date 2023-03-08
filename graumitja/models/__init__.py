08391a19e0dba78730f6aeb980ad13dbe31cb41f
Iohel <joelpons9@hotmail.com>
2023-03-08 19:45:59 +0100

    Exercici Institut

---
+++ graumitja/models/__init__.py
@@ -0,0 +1,7 @@
+# -*- coding: utf-8 -*-
+# Aqui indicamos que se cargara el fichero "biblioteca_comic.py"
+# Si creamos mas modelos, deben importarse en este fichero
+from . import institut_cicle
+from . import institut_modul
+from . import institut_alumne
+from . import institut_professor
